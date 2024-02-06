9cf00c2e639858cce3aaf5552cb3a1d4ae79506e
mpinheirodev <devmatheusp97@gmail.com>
2024-02-06 01:07:07 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import Logo from "./assets/logo-nlw-experts.svg";
+import { NoteCard } from "./components/note-card";
+import { NewNoteCard } from "./components/new-note-card";
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={Logo} alt="NLW Experts" />
+
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          type="text"
+          placeholder="Busque em suas notas..."
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
+}
